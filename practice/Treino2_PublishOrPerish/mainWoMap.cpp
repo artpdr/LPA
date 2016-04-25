376c5c85f6822f84e680e022fd72aa7d2373d6ef
Artur Pedroso <apedroso@student.dei.uc.pt>
2016-04-25 22:55:53 +0100

    Training problems

--- practice/Treino2_PublishOrPerish/mainWoMap.cpp
+++ practice/Treino2_PublishOrPerish/mainWoMap.cpp
@@ -1,13 +1,14 @@
+#include <iostream>
 #include <cstdlib>
 #include <cstdio>
-#include <map>
+#include <algorithm>
 
 struct Papers_Production{
-    //The key is the budget and the value is the number of papers
-    std::map <int, int> num_papers;
+    int budget;
+    int num_papers;
 };
 
-Papers_Production papers[200];
+Papers_Production papers[200][10];
 int dp[200][5001];
 int num_proposals[200];
 //int count = 0;
@@ -19,12 +20,10 @@ int max_papers(int person, int budget){
 
     if(person == 0){
         int tmp_max = 0;
-        std::map<int, int>::iterator it;
-        for (it = papers[person].num_papers.begin(); it != papers[person].num_papers.end(); it++){
-            if(it->first > budget)
-                break;
-            else if(it->second > tmp_max){
-                tmp_max = it->second;
+        for(int i = 0; i < num_proposals[person]; i++){
+            if(papers[person][i].budget <= budget){
+                if(papers[person][i].num_papers > tmp_max)
+                    tmp_max = papers[person][i].num_papers;
             }
         }
         return dp[person][budget] = tmp_max;
@@ -32,13 +31,11 @@ int max_papers(int person, int budget){
 
     int tmp_max = 0;
     int max_for_person = 0;
-    std::map<int, int>::iterator it;
+
     //with a project from this researcher
-    for (it = papers[person].num_papers.begin(); it != papers[person].num_papers.end(); it++){
-        if(it->first > budget)
-            break;
-        else{
-            tmp_max = it->second + max_papers(person-1, budget - it->first);
+    for(int i = 0; i < num_proposals[person]; i++){
+        if(papers[person][i].budget <= budget){
+            tmp_max = papers[person][i].num_papers + max_papers(person-1, budget-papers[person][i].budget);
             if(tmp_max > max_for_person)
                 max_for_person = tmp_max;
         }
@@ -58,6 +55,7 @@ int main()
     //freopen("test.txt", "r", stdin);
 
     int n, m;
+    int num_proposals_tmp[200];
     scanf("%d %d", &n, &m);
 
     for(int i = 0; i < n; i++){
@@ -67,20 +65,30 @@ int main()
 
     /*get num of proposals of each researcher*/
     for(int i = 0; i < n; i++){
-        scanf("%d", &(num_proposals[i]));
+        scanf("%d", &(num_proposals_tmp[i]));
     }
 
     /*get max number of papers for each amount to every researcher*/
     int tmp_papers, tmp_amount;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < num_proposals[i]; j++){
+        num_proposals[i] = 0;
+        for(int j = 0; j < num_proposals_tmp[i]; j++){
             /*assuming that amount doesn't exceed the maximum amount*/
             scanf("%d %d", &tmp_amount, &tmp_papers);
             /*the researcher i may already have a proposal for this amount and we just want the one that produces more papers*/
-            if(papers[i].num_papers[tmp_amount] == 0)
-                papers[i].num_papers[tmp_amount] = tmp_papers;
-            else if(papers[i].num_papers[tmp_amount] < tmp_papers)
-                papers[i].num_papers[tmp_amount] = tmp_papers;
+            for(int k = 0; k < 10; k++){
+                if(papers[i][k].budget == 0){
+                    papers[i][k].budget = tmp_amount;
+                    papers[i][k].num_papers = tmp_papers;
+                    num_proposals[i]++;
+                    break;
+                }
+                if(papers[i][k].budget == tmp_amount){
+                    if(papers[i][k].num_papers < tmp_papers)
+                        papers[i][k].num_papers = tmp_papers;
+                    break;
+                }
+            }
         }
     }
 
